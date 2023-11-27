68348429a6df30d8527f23e5b0f684c9b0311129
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2023-11-27 12:23:50 +0100

    Fixes

--- gemm_benchmarks.jl
+++ gemm_benchmarks.jl
@@ -18,17 +18,19 @@ const cpu_types = [Float32, Float64]
 const gpu_types = [BFloat16, Float16, Float32, Float64]
 const gpu_modes = [CUDA.DEFAULT_MATH, CUDA.FAST_MATH]
 
-function rand_xpu(device, args...)
-    if device == :cpu 
+abstract type Device end
+struct CPU <: Device end
+struct GPU <: Device end
+
+function rand(dev::Device, args...)
+    if dev isa CPU
         rand(args...)
-    elseif device == :gpu
+    elseif dev isa GPU 
         CUDA.rand(args...)
-    else
-        throw(ArgumentError("Device must be either :gpu or :cpu, got $device"))
     end
 end
 
-random_matrix(T, size, device) = 2rand_xpu(device, T, size, size) .- one(T)
+random_matrix(T, size, device) = 2rand(device, T, size, size) .- one(T)
 gemm_init(T, size, device) = random_matrix(T, size, device), random_matrix(T, size, device)
 
 if abspath(PROGRAM_FILE) == @__FILE__
@@ -39,13 +41,13 @@ if abspath(PROGRAM_FILE) == @__FILE__
 
     suite["cpu"] = BenchmarkGroup()
     for T in cpu_types, size in sizes
-        suite["cpu"][T, size] = @benchmarkable A * B setup=((A, B) = gemm_init($T, $size, :cpu))
+        suite["cpu"][T, size] = @benchmarkable A * B setup=((A, B) = gemm_init($T, $size, CPU()))
     end
 
     for mode in [CUDA.DEFAULT_MATH, CUDA.FAST_MATH]
         suite["gpu", string(mode)] = BenchmarkGroup()
         for T in [BFloat16, Float16, Float32, Float64], size in sizes
-            suite["gpu", string(mode)][T, size] = @benchmarkable (CUDA.@sync A * B) setup=((A, B) = gemm_init($T, $size, :gpu); CUDA.math_mode!($mode))
+            suite["gpu", string(mode)][T, size] = @benchmarkable (CUDA.@sync A * B) setup=((A, B) = gemm_init($T, $size, GPU()); CUDA.math_mode!($mode))
         end
     end
 
